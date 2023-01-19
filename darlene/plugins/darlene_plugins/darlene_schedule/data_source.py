70b709970c51d9125e2ca9adb78198b4267f3710
yinghanwen <yinghanwen2007@gmail.com>
2023-01-19 22:19:26 +0800

    fix: fixed schedule's get words function

--- darlene/plugins/darlene_plugins/darlene_schedule/data_source.py
+++ darlene/plugins/darlene_plugins/darlene_schedule/data_source.py
@@ -5,27 +5,27 @@ from utils.json_utils import JsonUtils
 from .config import WORDS_PATH, Path
 
 async def get_wake_up_msg() -> Message:
-    wake_up_json = Path(WORDS_PATH / "wake_up.json")["words"]
+    wake_up_json = Path(WORDS_PATH / "wake_up.json")
     words = JsonUtils.read_json(wake_up_json)
     word = Message(choice(words))
     return word
 
 
 async def get_lunch_msg() -> Message:
-    lunch_json = Path(WORDS_PATH / "lunch.json")["words"]
+    lunch_json = Path(WORDS_PATH / "lunch.json")
     words = JsonUtils.read_json(lunch_json)
     word = Message(choice(words))
     return word
 
 
 async def get_dinner_msg() -> Message:
-    dinner_json = Path(WORDS_PATH / "dinner.json")["words"]
+    dinner_json = Path(WORDS_PATH / "dinner.json")
     words = JsonUtils.read_json(dinner_json)
     word = Message(choice(words))
     return word
 
 async def sleep_msg() -> Message:
-    lunch_json = Path(WORDS_PATH / "lunch.json")["words"]
+    lunch_json = Path(WORDS_PATH / "lunch.json")
     words = JsonUtils.read_json(lunch_json)
     word = Message(choice(words))
     return word
